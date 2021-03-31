656a0e4f1ac794a3469919ce13b9dc26c6f832fd
jawblia <juliabell021@gmail.com>
2021-03-31 07:38:36 -0400

    test for npm

---
+++ src/App.js
@@ -0,0 +1,10 @@
+
+function App() {
+  return (
+    <div className="App">
+      <h1>npm export</h1>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/lib/components/Test.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Test = (props) => {
+    return (
+        <div>
+            <h3 style={{color:props.color}}>
+                Hey!!!
+            </h3>
+        </div>
+    );
+}
+ 
+export default Test;
